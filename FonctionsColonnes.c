283616d86d5d65850bfc9417c08b60cbae583791
Sidnehh <SidneySanglier13pgmail.com>
2024-03-28 23:33:25 +0100

    update : Parties 4.1-4 et 4.1-5

--- FonctionsColonnes.c
+++ FonctionsColonnes.c
@@ -6,3 +6,48 @@
 //PARTIE SABRINA
 
 //PARTIE SIDNEY
+
+void Print_col(struct COLUMN* col)
+{
+    int i;
+    for(i=0; i<col->TL; i++)
+    {
+        printf("[%d] %d", i, col->T[i]);
+    }
+    printf("\n");
+}
+
+int Frequency(struct COLUMN* col, int val)
+{
+    int i, sum=0;
+    for(i=0; i<col->TL; i++)
+    {
+        sum += (col->T[i] == val);
+    }
+    return sum;
+}
+
+int Val_From_Id(struct COLUMN* col, int id)
+{
+    return col->T[id];
+}
+
+int Nb_Superior(struct COLUMN* col, int val)
+{
+    int i, sum=0;
+    for(i=0; i<col->TL; i++)
+    {
+        sum += (col->T[i] > val);
+    }
+    return sum;
+}
+
+int Nb_Inferior(struct COLUMN* col, int val)
+{
+    int i, sum=0;
+    for(i=0; i<col->TL; i++)
+    {
+        sum += (col->T[i] < val);
+    }
+    return sum;
+}
