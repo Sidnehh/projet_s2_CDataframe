9748b785509cfd716d6224e714e79c75e7264ba3
ElhSabrina <150938222+ElhSabrina@users.noreply.github.com>
2024-05-20 20:22:46 +0200

    Update FonctionsCDataFrame.h

--- FonctionsCDataFrame.h
+++ FonctionsCDataFrame.h
@@ -8,13 +8,15 @@ COLUMN** create_empty_cdataframe(int nb);
 void fill_cdataframe(COLUMN** cdf, int TL);
 // fonction qui remplit le cdataframe
 int maxcol_cdataframe(COLUMN** cdf, int TL);
-//
+// fonction qui 
 void print_cdataframe(COLUMN** cdf, int TL);
 // fonction qui affiche le cdataframe
 void print_lines_cdataframe(COLUMN** cdf, int TL, int nlines);
-
+//fonction qui affiche des lignes données par l'utilisateur 
 void print_columns_cdataframe(COLUMN** cdf, int TL, int ncol);
-
+//fonction qui affiche des colonnes données par l'utilisateur 
 void add_col_cdataframe(COLUMN** cdf, int* TL, COLUMN* col);
+// fonction qui ajoute une colonne au cdataframe
 void delete_column_cdf(COLUMN*** cdataframe, int* TL, int index);
+//fonction qui supprime une colonne 
 #endif PROJET_S2_CDATAFRAME_FONCTIONSCDATAFRAME_H
