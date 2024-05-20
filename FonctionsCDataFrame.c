79b71a5715d1382bea63637c7d9cacae19d1931f
sabrina <sabrina.el-hassani@efrei.net>
2024-05-20 23:33:34 +0200

    ggg

---
+++ FonctionsCDataFrame.c
@@ -0,0 +1,249 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "FonctionsColonnes.h"
+
+// Fonction qui crée une nouvelle liste et renvoie un pointeur vers celle-ci
+LIST *create_list(void)
+{
+    LIST *list = (LIST*) malloc(sizeof(LIST)); // Allocation dynamique pour une structure 'LIST'
+    if (list) {
+        list->head = NULL; // Initialise l'attribut head à NULL
+        list->tail = NULL; // Initialise l'attribut tail à NULL
+    }
+    return list; // Retourne le pointeur vers la liste créée
+}
+
+// Fonction qui crée un nouveau nœud de liste avec une colonne donnée
+LNODE* creer_lnode(COLUMN *column)
+{
+    LNODE* lnode= (LNODE*)malloc(sizeof(LNODE)); // Allocation dynamique de mémoire pour une structure LNODE
+    if (lnode == NULL)
+    {
+        printf("Erreur d'allocation"); // Message d'erreur en cas d'échec de l'allocation
+        return NULL;
+    }
+    lnode->prev = NULL; // Initialise l'attribut prev à NULL
+    lnode->next = NULL; // Initialise l'attribut next à NULL
+    lnode->data = column; // Affecte la colonne au champs data
+    return lnode; // Retourne le pointeur vers le nœud créé
+}
+
+// Fonction qui crée un dataframe contenant plusieurs colonnes
+CDATAFRAME *create_cdataframe(ENUM_TYPE *cdftype, int size)
+{
+    LIST *cdf = create_list(); // Création d'une nouvelle liste pour le dataframe
+    if (cdf == NULL)
+    {
+        printf("Erreur d'allocation"); // Message d'erreur en cas d'échec de l'allocation
+        free(cdf); // Libération de la mémoire allouée
+        return NULL;
+    }
+    else
+    {
+        for (int i = 0; i < size; i++) { // Boucle pour créer et ajouter les colonnes
+            char *titre = (char *) malloc(100 * sizeof(char)); // Allocation dynamique pour le titre de la colonne
+            int type;
+            if (titre == NULL) {
+                free(cdf); // Libération de la mémoire allouée
+                printf("Erreur d'allocation"); // Message d'erreur en cas d'échec de l'allocation
+                return NULL;
+            }
+            printf("Saisir le titre de la colonne %d\n", i + 1);
+            scanf(" %s", titre); // Lecture du titre de la colonne
+            printf("Saisir le type de la colonne %d\n", i + 1);
+            printf("1 : entier naturel\n2 : entier\n3 : caractere\n4 : decimal\n5 : flottant\n6: chaine de caracteres\n");
+            scanf(" %d", &type); // Lecture du type de la colonne
+            COLUMN *column = create_column(type - 1, titre); // Création de la colonne
+            fill_column(column); // Remplissage de la colonne
+
+            LNODE *node = creer_lnode(column); // Création d'un nouveau nœud pour la colonne
+
+            if (column == NULL || node == NULL) {
+                printf("Erreur d'allocation"); // Message d'erreur en cas d'échec de l'allocation
+                free(titre); // Libération de la mémoire allouée pour le titre
+                free(column); // Libération de la mémoire allouée pour la colonne
+                free(node); // Libération de la mémoire allouée pour le nœud
+                free(cdf); // Libération de la mémoire allouée pour la liste
+                return NULL;
+            }
+            if (cdf->head == NULL) {
+                cdf->head = node; // Si la liste est vide, le nouveau nœud devient le premier
+                cdf->tail = node; // et le dernier nœud de la liste
+            } else {
+                node->prev = cdf->tail; // Le nœud précédent du nouveau nœud est l'ancien dernier nœud
+                cdf->tail->next = node; // Le nœud suivant de l'ancien dernier nœud est le nouveau nœud
+                cdf->tail = node; // Le nouveau nœud devient le dernier nœud de la liste
+            }
+        }
+    }
+    return cdf; // Retourne le pointeur vers le dataframe créé
+}
+
+// Fonction qui supprime et libère la mémoire allouée pour un dataframe
+void delete_cdataframe(CDATAFRAME** cdf)
+{
+    if (cdf == NULL || *cdf == NULL)
+        return;
+    LNODE* tmp = (*cdf)->head;
+    LNODE *current = tmp;
+    while (tmp != NULL)
+    {
+        current = current->next;
+        delete_column(&tmp->data); // Suppression de la colonne
+        tmp->prev = NULL;
+        free(tmp); // Libération de la mémoire allouée pour le nœud
+        tmp = current;
+    }
+    free(*cdf); // Libération de la mémoire allouée pour la liste
+    *cdf = NULL;
+}
+
+// Fonction qui affiche le dataframe
+void print_cdf(CDATAFRAME* cdf)
+{
+    if (cdf == NULL)
+    {
+        printf("le cdata est deja vide\n");
+        return;
+    }
+    int choice;
+    printf("Voulez vous afficher le Cdataframe dans le sens de remplissage (1) ou inverse (0) ?\n");
+    scanf(" %d", &choice);
+    switch (choice) {
+        case 0:
+        {
+            LNODE *current = cdf->head;
+            while (current != NULL)
+            {
+                print_col(current->data); // Affichage de la colonne
+                current = current->next; // Passage à la colonne suivante
+                printf("\n");
+            }
+            break;
+        }
+        case 1:
+        {
+            LNODE *current = cdf->tail;
+            while (current != NULL)
+            {
+                print_col(current->data); // Affichage de la colonne
+                current = current->prev; // Passage à la colonne précédente
+                printf("\n");
+            }
+            break;
+        }
+        default:
+            printf("Choix invalide\n");
+            break;
+    }
+}
+
+// Fonction qui supprime une colonne d'un dataframe à partir de son titre
+void delete_c(CDATAFRAME *cdf, char *titre)
+{
+    if (cdf == NULL)
+    {
+        printf("la colonne n'existe pas car le cdata est vide");
+        return;
+    }
+    LNODE* current = cdf->head;
+    while (current != NULL && strcmp(current->data->titre, titre) != 0) // Parcourt le cdataframe et
+        //tant que le titre n'est pas trouvé dans la colonne du noeud , on passe au noeud suivant
+
+    {
+        current = current->next;
+    }
+
+    if (current == NULL) {
+        printf("La colonne %s est introuvable\n", titre);
+        return;
+    }
+    if (current->prev != NULL)
+    {
+        current->prev->next = current->next;
+    }
+    else {
+        cdf->head = current->next;
+    }
+    if (current->next != NULL)
+    {
+        current->next->prev = current->prev;
+    }
+    else
+    {
+        cdf->tail = current->prev;
+    }
+
+    delete_column(&current->data); // Suppression de la colonne
+    free(current); // Libération de la mémoire allouée pour le nœud
+    printf("Colonne %s supprimee avec succes\n", titre);
+}
+
+// Fonction qui ajoute une colonne à une position donnée dans un dataframe
+void ajouter_column(CDATAFRAME *cdf, COLUMN* column, unsigned int index)
+{
+    LNODE *new_node = creer_lnode(column); // Création d'un nouveau nœud
+    if (cdf == NULL)
+    {
+        printf("cDataframe vide");
+        free(new_node); // Libération de la mémoire allouée pour le nouveau nœud
+        return;
+    }
+    if (index == 0 || cdf->head == NULL)
+    {
+        new_node->next = cdf->head; // Insertion au début de la liste
+        if (cdf->head != NULL)
+            cdf->head->prev = new_node; // Mise à jour du nœud suivant
+        cdf->head = new_node; // Mise à jour de la tête de liste
+        if (cdf->tail == NULL)
+            cdf->tail = new_node; // Si la liste était vide, mise à jour de la queue de liste
+        return;
+    }
+    LNODE* current = cdf->head;
+    int i = 0;
+    while (current != NULL && i < index - 1) // On s'arrête un indice avant l'insertion
+    {
+        current = current->next;
+        i++;
+    }
+    if (current == NULL || current->next == NULL) {
+        if (current != NULL) {
+            current->next = new_node;
+            new_node->prev = current;
+        }
+        else {
+            if (cdf->tail != NULL) {
+                cdf->tail->next = new_node;
+                new_node->prev = cdf->tail;
+            }
+            cdf->tail = new_node;  // affecte en dernière position si dépassement lors de la boucle
+        }
+    }
+    else {
+        // Insertion au milieu
+        new_node->next = current->next; // Le nœud suivant du nouveau nœud est celui après current
+        new_node->prev = current; // Le nœud précédent du nouveau nœud est current
+        if (current->next != NULL) {
+            current->next->prev = new_node;
+        }
+        current->next = new_node;
+    }
+}
+
+// Fonction qui renvoie le nombre de colonnes dans un dataframe
+int get_cdataframe_cols_size(CDATAFRAME *cdf)
+{
+    if (cdf == NULL)
+        return 0; // Retourne 0 si le dataframe est vide
+    LNODE* current = cdf->head; // Pointeur vers le premier nœud de la liste
+    int count = 0; // Initialisation du compteur
+    while (current != NULL)
+    {
+        count++; // Incrémente le compteur
+        current = current->next; // Passe au nœud suivant
+    }
+    return count;
+}
+
+
--- FonctionsColonnes.h
+++ FonctionsColonnes.h
@@ -1,86 +1,21 @@
-#ifndef TEST__LISTE_CHAÎNÉ_H
-#define TEST__LISTE_CHAÎNÉ_H
-
-typedef enum {
-    UINT,
-    INT,
-    CHAR,
-    FLOAT,
-    DOUBLE,
-    STRING,
-} ENUM_TYPE;
-
-typedef union column_type{
-    unsigned int uint_value;
-    signed int int_value;
-    char char_value;
-    float float_value;
-    double double_value;
-    char* string_value;
-    void* struct_value;
-}COL_TYPE ;
-
-typedef struct
-{
-    char *titre;
-    int taille_logique;
-    int taille_physique;
-    unsigned long long int* index;
-    COL_TYPE **data;
-    ENUM_TYPE type;
-} COLUMN;
-
-typedef struct lnode {
-    COLUMN *data; // Pointer to a column
-    struct lnode *prev;
-    struct lnode *next;
-}LNODE;
-
-typedef struct {
-    LNODE *head;
-    LNODE *tail;
-} LIST ;
-typedef LIST CDATAFRAME;
 
 
-LIST *create_list(void);
-//fonction crée un pointeur de type liste
+#ifndef TEST__COLONNES_H
+#define TEST__COLONNES_H
 
-LNODE* creer_lnode(COLUMN *column);
-// fonction qui crée un noeud
+#include "FonctionsCDataFrame.h"
 
 COLUMN *create_column(ENUM_TYPE type, char *title);
-// fonction qui créer une colonne de n'importe quel type
-
-CDATAFRAME *create_cdataframe( ENUM_TYPE *cdftype, int size);
-// fonction qui crée un cdataframe
+// fonction qui crée une colonne de n'importe quel type
 
 void delete_column(COLUMN**column);
 // fonction qui libère une colonne
 
-void delete_c (CDATAFRAME *cdf, char *titre);
-// fonction qui supprime une colonne d'un cdataframe à partir de son titre
-
 void print_col(COLUMN*column);
 //fonction qui affiche une colonne
 
-void delete_cdataframe(CDATAFRAME** cdf);
-// fonction qui libère un cdataframe
-
-void ajouter_column( CDATAFRAME*cdf, COLUMN*column, unsigned int index);
-// fonction qui permet d'ajouter un noeud/ colonne dans le cdataframe (specifier l'indice)
-
-void print_cdf(CDATAFRAME* cdf);
-//fonction qui affiche un cdataframe
-
-int get_cdataframe_cols_size(CDATAFRAME *cdf);
-//fonction qui renvoie le nombre de colonnes d'un cdataframe
-
 void convert_value(COLUMN *col, unsigned long long int i, char *str, int size);
 // fonction qui convertit une variable de l'indice i d'une colonne en chaîne de caractères
-
-void print_col(COLUMN* col);
-//fonction qui affiche une colonne
 
 int fill_column(COLUMN* col);
 //fonction qui remplit une colonne de manière interactive
@@ -90,7 +25,9 @@ int insert_value(COLUMN* col, void* value);
 //bien déroulée, 0 sinon
 
 int is_digit(char*);
-int convert_to_int(char *str);
-//fonctions qui servent à vérifier les saisies utilisateur
+// fonction qui sert à verifier la saisie de l'utilisateur
 
-#endif //TEST__LISTE_CHAÎNÉ_H
+int convert_to_int(char *str);
+//fonctions qui servent à verifier les saisies utilisateur
+
+#endif //TEST__COLONNES_H
